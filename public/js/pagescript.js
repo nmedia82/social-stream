498604531e72e11654a0fe6fcbd6b7dfad9f84b8
rameez-nmedia <rameez.nmedia@gmail.com>
2016-11-28 21:07:35 +0500

    frontend created

---
+++ public/js/pagescript.js
@@ -0,0 +1,15 @@
+jQuery(document).ready(function($) {
+	$('[data-toggle="tooltip"]').tooltip();
+	// mixItUp function
+	$('#sortable-columns').mixItUp({
+        activeClass: 'on',
+        callbacks: { 
+            onMixStart: function(state){
+                $('#sortable-columns').masonry('destroy');
+            },
+            onMixEnd: function(state){
+                $('#sortable-columns').masonry();
+            },
+        }
+	});
+});
