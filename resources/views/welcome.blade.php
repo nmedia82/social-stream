85f598e1dc37649db0cfc924e8750cc879e05a2c
rameez-nmedia <rameez.nmedia@gmail.com>
2016-12-28 21:17:20 +0500

    social stream

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -76,6 +76,9 @@
 
                             <ul class="dropdown-menu" role="menu">
                                 <li>
+                                    <a href="{{ url('/home') }}">Dashboard</a>
+                                </li>
+                                <li>
                                   <a href="{{ url('/logout') }}"
                                       onclick="event.preventDefault();
                                                document.getElementById('logout-form').submit();">
